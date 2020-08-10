caf0032baf720af99ae456cc015b4e01afecb1c5
lively-system-6174 <wkalada1@gmail.com>
2020-08-10 02:01:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |other_keys, other_values|
       if value <= other_values
         lowest_value_key = key
-      else 
-        lowest_value_key = other_keys
+      
       end
       
     end
