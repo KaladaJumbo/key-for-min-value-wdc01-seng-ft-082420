c16f14fdea12a46eeb5034835cb3b6e33359081b
lively-system-6174 <wkalada1@gmail.com>
2020-08-10 02:15:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,25 @@
 
 def key_for_min_value(name_hash)
   
-  
+  lowest_value = 100000000000000000
   name_hash.each do |key, value|
     name_hash.each do |other_keys, other_values|
       if value <= other_values
-        lower_key = key
+        lower_value = value
       
       end
       
-      if lowest_value_key <= lower_key
-        lowest_value_key = lower_key
+      if lowest_value <= lower_value
+        lowest_value = lower_value
       end
       
     end
     
   end
+  
+  name_hash.each do |key, value|
+    if value == lowest_value
+      lowest_value_key = key
   
   return lowest_value_key
   
