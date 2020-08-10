1bdd67075af9a4d221442587924b8f86fe78b6b0
lively-system-6174 <wkalada1@gmail.com>
2020-08-10 02:38:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,40 @@
 
 def key_for_min_value(name_hash)
  
- if name_hash == {}
+  if name_hash == {}
    return nil
   else 
     
-    name_hash.each do |
+    low_age = 10000000
+    lowest_age = low_age
+    name_hash.each do |name, age|
+      
+      name_hash do |compared_names, compared_ages|
+        if compared_ages <= age
+          
+          low_age = compared_ages 
+          
+        end
+      
+      if low_age <= lowest_age
+          
+        lowest_age = low_age
+          
+      end 
+      
+    end
+    
+    name_hash.each do |name, age|
+      
+      if age == lowest_age
+        lowest_name = name
+      end 
+      
+    end
+    
+    return lowest_name
     
     
-end
+  end
  
 end
