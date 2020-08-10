8983d41275a7f2ab5e41253900b8d16be0d1fc53
lively-system-6174 <wkalada1@gmail.com>
2020-08-10 02:44:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -31,12 +31,12 @@ def key_for_min_value(name_hash)
     name_hash.each do |name, age|
       
       if age == lowest_age
-        lowest_name = name
+        lowest = name
       end 
       
     end
     
-    return lowest_name
+    return lowest
     
     
   end
